c2603b631e686790cb9e7e15b053d4e2cbe8dc76
Mark Gibson <jollytoad@gmail.com>
2009-05-14 11:43:09 +0100

    minor comment/message changes

--- jslint/rhino.js
+++ jslint/rhino.js
@@ -6,12 +6,14 @@ Copyright (c) 2002 Douglas Crockford  (www.JSLint.com) Rhino Edition
 
 // This is the Rhino companion to fulljslint.js.
 
+// 2009-05-14: Modifications by Mark Gibson to support multiple files.
+
 /*extern JSLINT */
 /*jslint rhino: true*/
 
 (function (a) {
 	if (!a[0]) {
-		print("Usage: jslint.js file.js [file.js ...]");
+		print("Usage: jslint.js file.js [...]");
 		quit(1);
 	}
 	for (var f = 0; f < a.length; f += 1) {
@@ -25,7 +27,7 @@ Copyright (c) 2002 Douglas Crockford  (www.JSLint.com) Rhino Edition
 				var e = JSLINT.errors[i];
 				if (e) {
 					print('jslint: ' + a[f]);
-					print('Lint at line ' + (e.line + 1) + ' character ' +
+					print('line ' + (e.line + 1) + ', character ' +
 							(e.character + 1) + ': ' + e.reason);
 					print((e.evidence || '').
 							replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
@@ -34,8 +36,9 @@ Copyright (c) 2002 Douglas Crockford  (www.JSLint.com) Rhino Edition
 			}
 			quit(2);
 		} else {
-			print("jslint: " + a[f] + " - good.");
+			print("jslint: " + a[f] + " - no problems");
 		}
 	}
+	quit();
 }(arguments));
 
